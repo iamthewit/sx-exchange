6ef815f3585bd1a3f90f079f28d44e3ff46bc454
Ben Cross <benross86@gmail.com>
2022-07-25 16:39:46 +0100

    Added incomplete exchange tests

---
+++ tests/Domain/ExchangeTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Tests\Domain;
+
+use StockExchange\Domain\Exchange;
+use PHPUnit\Framework\TestCase;
+
+class ExchangeTest extends TestCase
+{
+
+    public function testBid()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRestoreFromValues()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRemoveBid()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRemoveAsk()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testAsk()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testCreate()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testRestoreStateFromEvents()
+    {
+        $this->markTestIncomplete();
+    }
+
+    public function testItExecutesATrade()
+    {
+        $this->markTestIncomplete();
+    }
+}
