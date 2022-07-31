c92195eb14fc601983f927f363b8afd75d377d6a
Ben Cross <benross86@gmail.com>
2022-07-31 22:04:35 +0100

    Added tradeexecuted test

--- tests/Domain/ExchangeTest.php
+++ tests/Domain/ExchangeTest.php
@@ -4,14 +4,16 @@ namespace App\Tests\Domain;
 
 use Ramsey\Uuid\Uuid;
 use StockExchange\Domain\Event\ExchangeCreated;
+use StockExchange\Domain\Event\TradeExecuted;
 use StockExchange\Domain\Exchange;
 use PHPUnit\Framework\TestCase;
 use StockExchange\Domain\Price;
 use StockExchange\Domain\Symbol;
+use StockExchange\Domain\Trade;
 
 class ExchangeTest extends TestCase
 {
-    public function testItCreatesanExchange()
+    public function testItCreatesAnExchange()
     {
         $exchangeId = Uuid::uuid4();
         $exchange = Exchange::create($exchangeId);
@@ -87,7 +89,42 @@ class ExchangeTest extends TestCase
 
     public function testItExecutesATrade()
     {
-        $this->markTestIncomplete();
+        $exchangeId = Uuid::uuid4();
+        $exchange = Exchange::create($exchangeId);
+        
+        $askId = Uuid::uuid4();
+        $exchange->ask(
+            $askId,
+            Uuid::uuid4(),
+            Symbol::fromValue('FOO'),
+            Price::fromValue(100)
+        );
+
+        $bidId = Uuid::uuid4();
+        $exchange->bid(
+            $bidId,
+            Uuid::uuid4(),
+            Symbol::fromValue('FOO'),
+            Price::fromValue(100)
+        );
+
+        $this->assertCount(1, $exchange->trades());
+        $this->assertCount(0, $exchange->bids());
+        $this->assertCount(0, $exchange->asks());
+
+        /** @var Trade $trade */
+        $trade = $exchange->trades()->getIterator()->current();
+        $this->assertEquals($askId->toString(), $trade->ask()->id()->toString());
+        $this->assertEquals($bidId->toString(), $trade->bid()->id()->toString());
+
+        $found = false;
+        foreach ($exchange->dispatchableEvents() as $dispatchableEvent) {
+            if(get_class($dispatchableEvent) === TradeExecuted::class) {
+                $found = true;
+                break;
+            }
+        }
+        $this->assertTrue($found);
     }
 
     public function testToArray()
